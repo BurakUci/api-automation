a74491d02928dc59a5da18bd5424134e577bac8d
Burakuci <ucibrk@gmail.com>
2025-04-10 18:59:21 +0300

    SoftAssert

---
+++ src/test/java/tests/P10_Practice.java
@@ -0,0 +1,20 @@
+package tests;
+
+public class P10_Practice {
+
+    /*
+ https://reqres.in/api/users url'ine
+     bir GET request yolladigimizda
+     donen Response'in
+     status code'unun 200,
+     ve content type'inin Aplication.JSON,
+     ve response body'sindeki
+     user sayisinin 6
+     ve kayıtlardan birinin adını "Emma" oldugunu test edin.
+*/
+
+
+
+
+
+}
